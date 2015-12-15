aae86e8e571ddc77313c2475c5d5f968c70b46cb
maydaykai <285643818@qq.com>
2015-12-15 23:01:40 +0800

    V0.6会员管理

---
+++ APICloud/DataConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APICloud.Rest;
+
+namespace APICloud
+{
+    public class DataConstructor
+    {
+        public static Factory Factory(string modelName)
+        {
+            var resouce = new Resource("A6994668718085", "664D29B1-760E-1B76-5871-CEB71FB2D993");
+            return resouce.Factory(modelName);
+        }
+        public static Resource Resource()
+        {
+            return new Resource("A6994668718085", "664D29B1-760E-1B76-5871-CEB71FB2D993");
+        }
+    }
+}
---
+++ WebUI/API/Member.svc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using APICloud;
+
+namespace WebUI.API
+{
+    [ServiceContract(Namespace = "WebUI.API")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Member
+    {
+        // 要使用 HTTP GET，请添加 [WebGet] 特性。(默认 ResponseFormat 为 WebMessageFormat.Json)
+        // 要创建返回 XML 的操作，
+        //     请添加 [WebGet(ResponseFormat=WebMessageFormat.Xml)]，
+        //     并在操作正文中包括以下行:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        public void DoWork()
+        {
+            // 在此处添加操作实现
+            return;
+        }
+
+        // 在此处添加更多操作并使用 [OperationContract] 标记它们
+        [OperationContract]
+        [WebInvoke(ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.WrappedRequest, Method = "GET")]
+        public string GetList(string sidx, string sord, int page, int rows, int memberID, string startDate, string endDate, string title)
+        {
+            var model = DataConstructor.Factory("user");
+            return model.Query();
+        }
+    }
+}
