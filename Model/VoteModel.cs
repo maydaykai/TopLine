7f90eb2639456bdf6af32cfd2962f7966b499364
maydaykai <285643818@qq.com>
2016-02-01 18:11:50 +0800

    V0.9.9.2

---
+++ Model/VoteModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class VoteModel
+    {
+        /// <summary>
+        /// 编号
+        /// </summary>		
+        public string id { get; set; }
+
+        /// <summary>
+        /// 名称
+        /// </summary>		
+        public string title { get; set; }
+
+        /// <summary>
+        /// 图片
+        /// </summary>		
+        public string img { get; set; }
+
+        /// <summary>
+        /// 开始日期
+        /// </summary>		
+        public DateTime beginDate { get; set; }
+
+        /// <summary>
+        /// 结束日期
+        /// </summary>		
+        public DateTime endDate { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>		
+        public string desc { get; set; }
+        
+        /// <summary>
+        /// 状态 0：禁用；1：启用
+        /// </summary>		
+        public bool status { get; set; }
+
+        public string statusStr
+        {
+            get { return status ? "启用" : "禁用"; }
+        }
+    }
+}
---
+++ WebUI/HanderAshx/VoteManage/VoteHandler.ashx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using APICloud;
+using Model;
+using Newtonsoft.Json;
+
+namespace WebUI.HanderAshx.VoteManage
+{
+    /// <summary>
+    /// VoteHandler 的摘要说明
+    /// </summary>
+    public class VoteHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Buffer = true;
+            context.Response.ExpiresAbsolute = DateTime.Now.AddSeconds(-1);
+            context.Response.Expires = 0;
+            context.Response.CacheControl = "no-cache";
+            context.Response.AddHeader("Pragma", "No-Cache");
+            context.Response.ContentType = "text/json";
+            context.Response.Write(GetDataList());
+        }
+
+        //获取数据
+        public Object GetDataList()
+        {
+            var model = DataConstructor.Factory("vote");
+            var data = model.Query();
+            var list = JsonConvert.DeserializeObject<List<VoteModel>>(data);
+            var modelData = (from voteModel in list
+                             select new
+                             {
+                                 ID = voteModel.id,
+                                 Title = voteModel.title
+                             });
+            var jsonData = new
+            {
+                TotalRows = list.Count,//记录数
+                Rows = modelData//实体列表
+            };
+            return JsonConvert.SerializeObject(jsonData);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
